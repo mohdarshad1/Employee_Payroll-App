8e7944356e74b2bd7d6d77c20f88b4219b9d9f45
mohdarshad1 <mdarshad181@gmail.com>
2021-06-05 19:13:58 +0530

    [Mohd Arshad] : Remove Employee Payroll Services

--- js/siteProperties.js
+++ js/siteProperties.js
@@ -1,7 +1,6 @@
 let site_properties = {
+    use_local_storage: "false",
     home_page: "../pages/home.html",
     add_emp_payroll_page: "../pages/EmployeePayroll.html",
-    use_local_storage: "false",
-    server_url: "http://127.0.0.1:3000/employee/",
-    delete_server_url: "http://127.0.0.1:3000/employee/"
+    server_url: "http://localhost:3000/EmployeePayrollDB/"
 };
